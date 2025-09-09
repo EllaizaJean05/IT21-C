e9fc56c6f027b610018a3862325155ef10920630
Ellaiza Jean Balatero <ejbalatero05@gmail.com>
2025-09-09 09:45:47 +0800

    Added Student Variables

---
+++ BalateroLife.js
@@ -0,0 +1,6 @@
+let fname = "Ellaiza Jean Curay Balatero";
+let course = "Bachelor of Science in Information Technology";
+let year = "2nd Year";
+
+console.log("Name:" + fname + "\nCourse:" + course +"\nYear Level:" +year);
+
---
+++ index.js
@@ -0,0 +1,38 @@
+/*console.log("Hello World");
+
+let student = "Ellaiza Jean";
+let grade = 1.0;
+let isPassed = true;
+let address;
+let partner = null;
+
+console.log("Student Name:"+ student + "\nFinal Grade:" + grade + "\nPassed?:" + isPassed + "\nAddress:" + address + "\nPartner:" + partner);
+
+let num1 = 2;
+let num2 = 5;
+
+
+let sum = num1 + num2;
+let diff = num1 - num2;
+let prod = num1 * num2;
+let quo = num1 / num2;
+let mod = num1 % num2;
+
+console.log("Sum: " + sum + "\nDiffrence: " + diff + "\nProduct:" + prod + "\nQuotient:" + quo + "\nModule" + mod);
+*/
+/*let grade = 70;
+
+if (grade > 75){
+   console.log("Student passed!");
+} else {
+   console.log("Student Failed");
+} 
+*/
+
+// Loop
+for (let i  = 0; i<=5; i++){
+    console.log ("Count: "+i);
+    i++;
+
+
+}
