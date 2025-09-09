342d5003cfeafb15e0120586db573552430c59e1
Ellaiza Jean Balatero <ejabalatero05@gmail.com>
2025-09-10 07:03:15 +0800

    Added Operators For Daily Routine

--- BALATEROLIFE.js
+++ BALATEROLIFE.js
@@ -3,3 +3,15 @@ let course = "Bachelor of Science in Information Technology";
 let year = "2nd Year";
 
 console.log("Name:" + name + "\nCourse:" + course +"\nYear Level:" +year);
+
+
+
+let hours = 8;
+let study = 3;
+
+console.log("Total Hours of Sleep:" + hours + "\nTotal Hours of Study:"+ study);
+
+let studyHours = 3;
+if (studyHours >= 5) {
+    console.log("Great! You studied enough today.");
+}
