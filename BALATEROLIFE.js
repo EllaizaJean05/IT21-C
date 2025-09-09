d98ed2183fce48b36553d4808e539d5094d793a5
Ellaiza Jean Balatero <ejabalatero05@gmail.com>
2025-09-10 07:00:10 +0800

    Added Student Variables

---
+++ BALATEROLIFE.js
@@ -0,0 +1,5 @@
+let name = "Ellaiza Jean Curay Balatero";
+let course = "Bachelor of Science in Information Technology";
+let year = "2nd Year";
+
+console.log("Name:" + name + "\nCourse:" + course +"\nYear Level:" +year);
