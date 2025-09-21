dd0a02c4e2b6a563b93932791c88408c6a7a4739
Ellaiza Jean Balatero <ejabalatero05@gmail.com>
2025-09-21 13:18:37 +0800

    Added Encapsulation

--- encapsulation.js
+++ encapsulation.js
@@ -1,1 +1,22 @@
-class 
+
+class bankAccount{
+    #balance;
+
+    constructor(owner, balance){
+    this.owner = owner;
+    this.#balance = balance;
+    }
+   deposit(amount){
+    this.#balance +=amount;
+    console.log("Deposited: " + amount);
+   }
+//create object
+getBalance(){
+    return this.#balance;
+
+}
+}
+var acc = new bankAccount ("Ellaiza Jean C. Balatero", 10000);
+acc.deposit(20000);
+acc.deposit(5);
+console.log("Current Balance: " + acc.getBalance());
